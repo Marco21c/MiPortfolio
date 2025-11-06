11489fdd7cb5cfa48dc4e1bdd31534706e953e36
Marcos21c <marcosecondori@gmail.com>
2025-11-05 23:55:10 -0300

    Se agrego imgs a las cards

--- src/components/PortfolioContainer/PortfolioContainer.jsx
+++ src/components/PortfolioContainer/PortfolioContainer.jsx
@@ -1,5 +1,6 @@
 import './PortfolioContainer.css'
 import {useProjects} from '../../hooks/useProject'
+import ProyectoCardContainer from '../ProyectoCardContainer/ProyectoCardContainer'
 export default function PortfolioContainer() {
   const proyectos = useProjects()
   return (
@@ -8,20 +9,7 @@ export default function PortfolioContainer() {
              <h1>Proyectos Destacados</h1>
              <div className='card-container'>
             {proyectos.map((p)=>(
-             <div className="card text-bg-dark mb-3" >
-                 <div className="row g-0">
-                   <div className="col-md-4">
-                     <img src={p.imagen} className="img-fluid rounded-start" alt={p.nombre}/>
-                   </div>
-                   <div className="col-md-8">
-                     <div className="card-body ">
-                       <h5 className="card-title ">{p.nombre}</h5>
-                       <p className="card-text"> {p.descripcion}</p>
-                       <a type="button" className='btn btn-primary' target='_blanck' href={p.link}>Ver</a>
-                     </div>
-                   </div>
-                 </div>
-              </div>
+                <ProyectoCardContainer key={p.id} proyecto={p}/>
                )) }
             </div>
        </section>
---
+++ src/components/ProyectoCardContainer/ProyectoCardContainer.jsx
@@ -0,0 +1,21 @@
+
+export default function ProyectoCardContainer({proyecto}) {
+  return (
+    <>
+       <div className="card text-bg-dark mb-3" >
+                 <div className="row g-0">
+                   <div className="col-5 col-md-12">
+                     <img src={proyecto.imagen} className="img-fluid rounded-start" alt={proyecto.nombre}/>
+                   </div>
+                   <div className="col-7 col-md-12">
+                     <div className="card-body ">
+                       <h5 className="card-title ">{proyecto.nombre}</h5>
+                       <p className="card-text"> {proyecto.descripcion}</p>
+                       <a type="button" className='btn btn-primary' target='_blanck' href={proyecto.link}>Ver</a>
+                     </div>
+                   </div>
+                 </div>
+              </div>
+    </>
+  )
+}
