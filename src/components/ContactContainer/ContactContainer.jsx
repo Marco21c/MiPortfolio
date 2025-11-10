1c5a1b0cbbca41be783031fe0cb5309899631638
Marcos21c <marcosecondori@gmail.com>
2025-11-10 13:17:15 -0300

    Cambio de logo

--- src/components/ContactContainer/ContactContainer.jsx
+++ src/components/ContactContainer/ContactContainer.jsx
@@ -1,3 +1,4 @@
+import { Link } from 'react-router-dom'
 import './ContactContainer.css'
 
 export default function ContactContainer() {
@@ -6,7 +7,7 @@ export default function ContactContainer() {
         <ul>
         <a  href="https://www.linkedin.com/in/marcos-condori-23c/" target="_blank"><i className='bx bxl-linkedin-square' ></i></a>
         <a  href="https://github.com/Marco21c" target="_blank"><i className='bx bxl-github'></i></a>
-        <a  href="https://mail.google.com/mail/?view=cm&fs=1&to=marcoecondori@gmail.com&su=Contacto%20desde%20portafolio&body=Hola,%20me%20gustaría%20saber%20más%20sobre%20ti." target="_blank"> <i className='bx bxl-gmail' ></i></a>
+        <Link to='/Contacto' > <i className='bx bxl-gmail' ></i></Link>
         </ul> 
     </div>
   )
