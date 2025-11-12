4578e4e86fffb5b2c29477bafbf92a91c9433919
Marcos21c <marcosecondori@gmail.com>
2025-11-12 01:25:21 -0300

    Se agrego algunos cambios de estilo

--- src/components/PortfolioContainer/PortfolioContainer.jsx
+++ src/components/PortfolioContainer/PortfolioContainer.jsx
@@ -8,7 +8,9 @@ export default function PortfolioContainer() {
        <section className='section-portafolio'>
              <h1>Proyectos Destacados</h1>
              <p className='text-center subtitle'>A continuación, se presentan algunos de los proyectos que desarrollé 
-              durante mi formación y práctica profesional, aplicando diferentes tecnologías y metodologías de desarrollo web y software.</p>
+              durante mi formación y práctica profesional, aplicando diferentes tecnologías y metodologías de desarrollo web y software.
+              También podés explorar mis repositorios en <a href="https://github.com/Marco21c" target="_blank" className='linkG'>GitHub</a> y <a href="https://gitlab.com/Marcos21c" target="_blank" className='linkG'>GitLab</a>,
+              donde comparto código y experimentos sobre mis proyectos.</p>
              <div className='card-container'>
             {proyectos.map((p)=>(
                 <ProyectoCardContainer key={p.id} proyecto={p}/>
